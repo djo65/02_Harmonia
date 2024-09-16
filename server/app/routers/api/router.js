c3aacf8193e6f61f4e2ae2aa103452ec8a0fc12d
djo65 <joriscamus@hotmail.fr>
2024-09-16 17:30:54 +0200

    route programs ok

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Import And Use Routers Here
-/* ************************************************************************* */
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
 
-/* ************************************************************************* */
+const programsRouter = require("./programs/router");
+
+router.use("/programs", programsRouter);
 
 module.exports = router;
